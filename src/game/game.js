16c719fa48507d5cd3b1755ab32053f4582d1605
Natalia Colares <nataliacolares@MacBook-Pro-de-Natalia.local>
2017-04-20 21:53:52 -0300

    #3 Criaçao das funcoes para desenho do obstaculo quadrado

--- src/game/game.js
+++ src/game/game.js
@@ -1,23 +1,27 @@
+function drawSquare(ctx,marker,size) {
+  ctx.beginPath();
+  ctx.moveTo(marker.x, marker.y);
+  ctx.lineTo(marker.x+size, marker.y);
+  ctx.lineTo(marker.x+size, marker.y+size);
+  ctx.lineTo(marker.x, marker.y+size);
+  ctx.lineTo(marker.x, marker.y);
+  ctx.closePath();
+  ctx.fillStyle = "red";
+  ctx.fill();
+
+  ctx.fillStyle = "black";
+  ctx.fillText(marker.id,marker.x+(size/2),marker.y+(size/2));
+}
+
 function drawGame(markers) {
   var canvas = document.getElementById('gameField');
   var ctx = canvas.getContext('2d');
   ctx.fillStyle = 'grey';
   ctx.fillRect(0,0,640,480);
-  ctx.font = "25px URW Gothic L, Arial, Sans-serif";
+  ctx.font = "20px URW Gothic L, Arial, Sans-serif";
 
   for(var i=0;i<markers.length;i++) {
-    ctx.beginPath();
-    ctx.moveTo(markers[i].x, markers[i].y);
-    ctx.lineTo(markers[i].x+50, markers[i].y);
-    ctx.lineTo(markers[i].x+50, markers[i].y+50);
-    ctx.lineTo(markers[i].x, markers[i].y+50);
-    ctx.lineTo(markers[i].x, markers[i].y);
-    ctx.closePath();
-    ctx.fillStyle = "red";
-    ctx.fill();
-
-    ctx.fillStyle = "black";
-    ctx.fillText(markers[i].id,markers[i].x+25,markers[i].y+25);
+    drawSquare(ctx, markers[i], 50);
   }
   ctx.restore();
 }
